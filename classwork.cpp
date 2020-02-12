cb051b32b96f080fa74cd162bd80b91fdd8f41ae
Md Shohed <49224753+mdshohed@users.noreply.github.com>
2020-02-12 09:43:08 +0600

    Update classwork.cpp

--- classwork.cpp
+++ classwork.cpp
@@ -52,3 +52,75 @@ int main()
     search(x);
     return 0;
 }
+
+
+
+
+#include<bits/stdc++.h>
+using namespace std;
+int ara[100];
+int length = 0;
+
+int insert( int value)
+{
+    ara[length] = value;
+    length++;
+}
+
+int print()
+{
+    for ( int i = 0; i<length; i++) {
+        cout<<ara[i]<<" ";
+    }
+    cout<<endl;
+}
+
+int search(int item)
+{
+    for ( int i =0; i<length; i++) {
+        if ( item == ara[i]) {
+            return i;
+        }
+    }
+    return -1;
+
+}
+int delete_index ( int index)
+{
+    if ( index < 0 || index >=length ) return -1;
+    
+    ara[index] = ara[length-1];
+    length--;
+        
+    return 9999;
+}
+
+
+int main()
+{
+    int n,x;
+    cin>>n;
+    for( int i = 0; i<n; i++) {
+        cin>>x;
+        insert(x);
+    }
+    print();
+    int index =  search(5);
+    if (index == -1) {
+        cout<<"not found"<<endl;
+    }
+    else {
+        cout<<"found in index "<< index<<endl;
+    }
+    
+    int temp  = delete_index(1);
+    
+    if ( temp == -1 ) {
+        cout<<"invalid index"<<endl;
+    }
+    else {
+        cout<<"successfully deleted"<<endl;
+    }
+    print();
+}
+
