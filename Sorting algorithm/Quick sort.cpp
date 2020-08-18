ba407fcfdb7406fa6f29786c247faf1649a07bd9
Shohedul Islam <49224753+mdshohed@users.noreply.github.com>
2020-08-18 10:15:37 +0600

    Update Quick sort.cpp

--- Sorting algorithm/Quick sort.cpp
+++ Sorting algorithm/Quick sort.cpp
@@ -1,3 +1,20 @@
+ALGORITHM: 
+/*QUICKSORT(A, p, r)
+    if p<r
+        q = PARTITION(A, p, r)
+        QUICKSORT(A, P, q-1) 
+        QUICKSORT(A, q+1, r) 
+        
+PARTITION(A, p, r)
+    x = A[r] 
+    i = p-1; 
+    for j = p to r-1 
+        if A[j]<x 
+        i = i+1
+        exchange A[i] with A[j] 
+    exchange A[i+1] with A[r] 
+return i+1*/
+
 #include <bits/stdc++.h>
 using namespace std;
 
