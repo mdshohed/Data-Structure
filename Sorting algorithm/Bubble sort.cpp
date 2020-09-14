684b9c526e99d82ea3a1a66aa5579689c839a78d
Shohedul Islam <49224753+mdshohed@users.noreply.github.com>
2020-09-14 21:09:14 +0600

    Update Bubble sort.cpp

--- Sorting algorithm/Bubble sort.cpp
+++ Sorting algorithm/Bubble sort.cpp
@@ -1,1 +1,27 @@
 
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    //read("input.txt");
+
+    int n;
+    cin >> n;
+    int ara[100];
+
+    for (int i = 0; i<n; i++) cin >> ara[i];
+
+    for (int i = 0; i<n-1; i++){
+        for (int j =0 ; j<n-i-1; j++){
+            if ( ara[j] > ara[j+1] ) {
+                swap( ara[j],ara[j+1]);
+            }
+        }
+    }
+
+    for (int i = 0; i<n; i++){
+        cout << ara[i] << " ";
+    }
+    cout << endl;
+    return 0;
+}
