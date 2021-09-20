63b9cedcac6f4a003e2666683c54cf92087b1410
Shohedul Islam <mdshohed170@gmail.com>
2021-09-20 22:52:41 +0600

    Update Lower_bound.cpp

--- Searching Algorithm/Lower_bound.cpp
+++ Searching Algorithm/Lower_bound.cpp
@@ -1,5 +1,8 @@
 // equal or greater value return lower_bound.
+# Step 1: 
+int position = lower_bound(a.begin(),a.end(), val) - a.begin();
 
+# Step 2: 
 int lower_bound(vector<int> a, int x) {
     int l = -1, r = a.size(); 
     while( 1+l<r) {
