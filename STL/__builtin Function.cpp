c79f06b588ac53030cfd3fd3df124bf2003e8a41
Shohedul Islam <mdshohed170@gmail.com>
2021-10-19 19:53:22 +0600

    Update __builtin Function.cpp

--- STL/__builtin Function.cpp
+++ STL/__builtin Function.cpp
@@ -2,7 +2,7 @@
 using namespace std; 
 
 int main() {
-	std::vector<int> v = {3, 4, 5, 8, 25, 5, 7, 0};
+	std::vector<int> v = {3, 4, 5, 8, 25, 5, -4, 5};
 	int a = 16, b = 15; 
 	string s = "1234";
 
@@ -23,5 +23,19 @@ int main() {
 	if (it!=v.end()) cout << *it << endl; 
 	else cout << "Not found" << endl; 
 	reverse(v.begin(), v.end());  // vector reverse 
+
+	int AllOfPositiveOrNegative = all_of(v.begin(), v.end(),[](int x){
+		return x>0; 
+	});   // All of value is positive in the array then the ans is true
+	int AnyOfPositiveOrNegative = any_of(v.begin(), v.end(),[](int x){
+		return x>0; 
+	});		// Any of value is positive in the array then the ans is true 
+	int noneOfValuePositiveorNot = none_of(v.begin(), v.end(),[](int x){
+		return x>0; 
+	});  // none of value positive in the array then the ans is true
+	int All_value_evenORnot = all_of(v.begin(), v.end(),[](int x){
+		return x%2==0; 
+	});
+	cout << All_value_evenORnot << endl; 
 	return 0;
 }
